ef886e47547b220872b3923bedfa31914f9d2953
jjl <james@behemoth>
2017-03-13 15:29:02 +0100

    bumped cheshire

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [clj-http "3.4.1"]
-                 [cheshire "5.6.3"]
+                 [cheshire "5.7.0"]
                  [com.cemerick/url "0.1.1"]
                  [org.clojure/data.codec "0.1.0"]
                  [environ "1.1.0"]])
