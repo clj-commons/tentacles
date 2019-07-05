64a30feb9a22dce0e11eb8ece99b96f412a8090e
Reynald Borer <reynald.borer@paper.li>
2019-07-05 13:41:19 +0200

    Forgot to add back license into the project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject irresponsible/tentacles "0.6.5-SNAPSHOT"
   :description "Clojure library for working with the Github v3 API"
   :url "https://github.com/clj-commons/tentacles"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0" :scope "provided"]
                  [clj-http "3.9.1"]
                  [cheshire "5.8.1"]
