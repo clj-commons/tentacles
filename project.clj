0e72cf49cfbacd28ed696215b6ec655d2db4559a
Reynald Borer <reynald.borer@paper.li>
2019-10-01 20:39:01 +0200

    Bump to clojure 1.10.1 & cheshire 5.9.0

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "https://github.com/clj-commons/tentacles"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0" :scope "provided"]
+  :dependencies [[org.clojure/clojure "1.10.1" :scope "provided"]
                  [clj-http "3.9.1"]
-                 [cheshire "5.8.1"]
+                 [cheshire "5.9.0"]
                  [com.cemerick/url "0.1.1"]
                  [org.clojure/data.codec "0.1.1"]
                  [environ "1.1.0"]]
