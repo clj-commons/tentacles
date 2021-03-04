6ee760fdb18c25ed031c9ad470c05ae0bfb3ea67
Reynald Borer <reynald.borer@finity.ai>
2021-03-04 18:00:14 +0100

    Bump Clojure, clj-http, cheshire & environ versions

--- project.clj
+++ project.clj
@@ -3,11 +3,11 @@
   :url "https://github.com/clj-commons/tentacles"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.1" :scope "provided"]
-                 [clj-http "3.9.1"]
-                 [cheshire "5.9.0"]
+  :dependencies [[org.clojure/clojure "1.10.3" :scope "provided"]
+                 [clj-http "3.12.1"]
+                 [cheshire "5.10.0"]
                  [com.cemerick/url "0.1.1"]
                  [org.clojure/data.codec "0.1.1"]
-                 [environ "1.1.0"]]
+                 [environ "1.2.0"]]
   :pedantic? :abort
   :deploy-repositories {"releases" {:url "https://repo.clojars.org" :creds :gpg}})
