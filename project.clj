28c8121fa5a0ad084d6ecdc6b11a4feeb3303205
Reynald Borer <reynald.borer@finity.ai>
2021-12-09 14:06:55 +0100

    Version 0.6.10-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject irresponsible/tentacles "0.6.9"
+(defproject irresponsible/tentacles "0.6.10-SNAPSHOT"
   :description "Clojure library for working with the Github v3 API"
   :url "https://github.com/clj-commons/tentacles"
   :license {:name "Eclipse Public License"
